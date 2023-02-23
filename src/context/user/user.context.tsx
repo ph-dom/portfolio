b5fea2ae59386607623fecd9bfb99e663725a2fe
Pablo Henriquez <p.henriquezd@outlook.com>
2023-02-23 19:48:10 -0300

    se espera a cargar el usuario si esta logueado

--- src/context/user/user.context.tsx
+++ src/context/user/user.context.tsx
@@ -1,15 +1,18 @@
 import React, { type PropsWithChildren } from 'react'
 import type { User } from 'firebase/auth'
 import { onAuthChanged } from '../../config/firebase'
+import Loader from '../../shared/loader/loader.component'
 
 export const UserContext = React.createContext<User | null>(null)
 
 export default function UserProvider (props: PropsWithChildren): JSX.Element {
   const [user, setUser] = React.useState<User | null>(null)
+  const [loading, setLoading] = React.useState(true)
 
   const handleChangeUser = (user: User | null): void => {
     console.log('PASE POR AQUICHI', user)
     setUser(user)
+    setLoading(false)
   }
 
   React.useEffect(() => {
@@ -21,7 +24,7 @@ export default function UserProvider (props: PropsWithChildren): JSX.Element {
 
   return (
     <UserContext.Provider value={user}>
-      {props.children}
+      {loading ? <Loader /> : props.children}
     </UserContext.Provider>
   )
 }
--- src/pages/editor/editor.component.tsx
+++ src/pages/editor/editor.component.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Editor as DraftEditor, EditorState } from 'draft-js'
+import './editor.styles.scss'
 
 function Editor (): JSX.Element {
+  const [editorState, setEditorState] = React.useState<EditorState>(EditorState.createEmpty())
   return (
-    <h1>Editor</h1>
+    <DraftEditor editorState={editorState} onChange={setEditorState} />
   )
 }
 
